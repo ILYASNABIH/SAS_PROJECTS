a63c544662f89a1d7bbe2c9eca3bb375b5233a68
ILYASNABIH <IlyasNabih17@hotmail.com>
2026-09-18 16:01:37 +0100

    Day1 Commit

--- Day1/Count Vowels.js
+++ Day1/Count Vowels.js
@@ -1,7 +1,8 @@
-let str = "bounjour";
+let str = "bOunjour";
 let count = 0
 for (let i = 0; i < str.length; i++) { 
-if (str[i] === "a" || str[i] === "e" || str[i] === "o" || str[i] === "i" || str[i] === "u")
+if (str[i] === "a" || str[i] === "e" || str[i] === "o" || str[i] === "i" || str[i] === "u" 
+|| str[i] === "A" || str[i] === "E" || str[i] === "O" || str[i] === "I" || str[i] === "A")
 {count++;
 console.log (str[i])
 }
