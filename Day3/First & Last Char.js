e932bc03af8c9bf3f62cc19b17e82aaa46492a85
ILYASNABIH <IlyasNabih17@hotmail.com>
2026-09-23 19:59:59 +0000

    Day1 Commit

---
+++ Day3/First & Last Char.js
@@ -0,0 +1,7 @@
+let word = "YouCode"
+const firstletter = word[0]
+const lastletter = word[word.length - 1]
+function firstandlastchar () {    
+    return firstletter + " " + lastletter
+}
+console.log(firstandlastchar ())
